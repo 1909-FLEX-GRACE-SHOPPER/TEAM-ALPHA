81926c090273b22757088c76006ce3c06a4c9d28
Elham Amini <elhamfarvid@gmail.com>
2020-01-16 12:18:49 -0500

    server set up

---
+++ main.js
@@ -0,0 +1,10 @@
+const db = require('./server/db/database.js');
+const app = require('./server/index');
+const chalk = require('chalk');
+const PORT = 3000;
+db.sync().then(() => {
+  console.log(chalk.greenBright('db synced'));
+  app.listen(PORT, () =>
+    console.log(chalk.greenBright(`Application now listening on PORT ${PORT}`))
+  );
+});
