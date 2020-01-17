fae9fe35d9d66960168f6116581a31d53029c801
Jake Hertz <hertzjake@gmail.com>
2020-01-17 13:40:51 -0500

    more seeding, shoeSizes model, and user routes

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const db = require('./server/db/database.js');
 const app = require('./server/index');
 const chalk = require('chalk');
 const PORT = 3000;
-db.sync().then(() => {
-  console.log(chalk.greenBright('db synced'));
-  app.listen(PORT, () =>
-    console.log(chalk.greenBright(`Application now listening on PORT ${PORT}`))
-  );
-});
+db.sync()
+  .then(() => {
+    console.log(chalk.greenBright('db synced'));
+    app.listen(PORT, () =>
+      console.log(chalk.greenBright(`Application now listening on PORT ${PORT} at http://localhost:${PORT}`))
+    );
+  })
+  .catch(e => {
+    console.log(chalk.red('connection error', e));
+  });
--- server/db/product_models/shoeSize.js
+++ server/db/product_models/shoeSize.js
@@ -3,24 +3,31 @@ const db = require('../database');
 
 const { ENUM, INTEGER } = Sequelize;
 
-const Sizes = db.define('sizes', {
+const ShoeSizes = db.define('shoeSizes', {
     id: {
         type: INTEGER,
         primaryKey: true,
         validate: {
             min: 1,
-            max: 6,
+            max: 13,
         }
     },
     size: {
         type: ENUM,
         values: [
-            'XS',
-            'S',
-            'M',
-            'L',
-            'XL',
-            'XXL'
+            '6',
+            '6.5',
+            '7',
+            '7.5',
+            '8',
+            '8.5',
+            '9',
+            '9.5',
+            '10',
+            '10.5',
+            '11',
+            '11.5',
+            '12'
         ],
         allowNull: false,
         validate: {
@@ -29,4 +36,4 @@ const Sizes = db.define('sizes', {
     },
 }, {timestamps: false});
 
-module.exports = Sizes;
+module.exports = ShoeSizes;
