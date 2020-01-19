c66777db3da74edfa20af93e1cb7a0a2c784387a
Skipups <svetlana.rovinsky@gmail.com>
2020-01-19 16:39:01 -0500

    added routes for single product, still working on tile

--- app/components/root.jsx
+++ app/components/root.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import store from '../store';
 import Home from './Home';
+import ProductTile from './ProductTile';
 
 export default class Root extends Component {
   async componentDidMount() {
@@ -14,10 +15,11 @@ export default class Root extends Component {
       <Router>
         <main>
           <Switch>
-            <Route path='/' component = { Home } exact />
+            <Route path="/" component={Home} exact />
+            <Route path="/products/:id" component={ProductTile} exact />
           </Switch>
         </main>
       </Router>
-    )
-  };
-};
+    );
+  }
+}
