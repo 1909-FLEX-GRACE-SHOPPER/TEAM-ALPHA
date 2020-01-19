e4326273ef59e4132af8d079590bbf236bc8d0cf
Skipups <svetlana.rovinsky@gmail.com>
2020-01-19 16:39:01 -0500

    added routes for single product, still working on tile

--- app/redux/singleProduct.js
+++ app/redux/singleProduct.js
@@ -3,8 +3,35 @@ import thunk from 'redux-thunk';
 
 const initialState = {};
 
+const GOT_PRODUCT = 'GOT_PRODUCT';
+
+export function gotProduct(fetchedProduct) {
+  const action = {
+    type: GOT_PRODUCT,
+    payload: fetchedProduct
+  };
+  console.log('action gotProduct', action);
+  return action;
+}
+
+export function getProductThunk(productId) {
+  const productFunc = async dispatch => {
+    try {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      console.log('get product thunk', data);
+      dispatch(gotProduct(data));
+    } catch (er) {
+      console.log(er);
+    }
+  };
+  console.log('productFunc', productFunc);
+  return productFunc;
+}
+
 const productReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GOT_PRODUCT:
+      return action.payload;
     default:
       return state;
   }
