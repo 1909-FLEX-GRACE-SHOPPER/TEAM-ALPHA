6c52e4e5abf1c84c9eed6ae6a3759bb5efe51d46
hertzj <hertzjake@gmail.com>
2020-01-27 18:45:52 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const chalk = require('chalk');
 // these two are good for cookies
 // const cookieParser = require('cookie-parser');
 const session = require('express-session');
@@ -18,6 +19,11 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  console.log(chalk.cyan(`${new Date().toString()}: ${req.path}`));
+  next();
+});
+
 // authentication and cookies
 // app.use(cookieParser());
 app.use(
@@ -31,40 +37,29 @@ app.use(
 );
 
 // session logging
-// app.use((req, res, next) => {
-//   console.log('session', req.session);
-//   next();
-// });
+app.use((req, res, next) => {
+  console.log('session', req.session);
+  next();
+});
 
-app.post('/auth/login', (req, res, next) => {
-  Users.findOne({
-    where: req.body
-  })
+app.use((req, res, next) => {
+  Users.findByPk(req.session.userId)
     .then(userOrNull => {
-      if (!userOrNull) return res.sendStatus(401);
-      req.session.userId = userOrNull.id;
-      res.status(200).send(userOrNull);
-      // res
-      //   .status(200)
-      //   .send(userOrNull)
-      //   .redirect('/');
+      if (!userOrNull) req.loggedIn = false;
+      else {
+        req.loggedIn = true;
+        req.user = userOrNull;
+      }
+      next();
     })
-    .catch(next);
+    .catch(e => {
+      console.log('error searching for a user by session.userId');
+      console.error(e);
+      next();
+    });
 });
 
-app.post('/auth/signup', (req, res, next) => {
-  Users.findOrCreate({
-    where: req.body
-  })
-    .then(user => {
-      if (!user) return res.status(500).send('error creating user');
-      req.session.userId = user.id;
-      res.send(user);
-    })
-    .catch(next);
-});
-
-// write logout route
+app.use('/auth', require('./auth'));
 
 // static middleware
 app.use(express.static(path.join(__dirname, '../public')));
@@ -72,15 +67,15 @@ app.use(express.static(path.join(__dirname, '../public')));
 // got to use those routes!
 app.use('/api', require('./api'));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 // handling errors
 app.use((err, req, res, next) => {
   // this is for testing; so if we don't have tests we can remove it
   if (process.env.NODE_ENV !== 'test') console.error(err.stack);
   res.status(err.status || 500).send(err.message || 'Internal server error');
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = app;
